b51d766c0183cc8b0f12dcac003f72f361812ff6
benni-bit <benjaminotto.1@web.de>
2025-01-08 08:09:23 +0000

    08/1.Kapselung von Codeblöcken

---
+++ src/08_funktionen_I.js
@@ -0,0 +1,14 @@
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+//Funktionsaufruf |call
+test()
+
+
+//Funktionsrumpf | callee
+//Funktionsdeklaration
+
+function test()
+{
+console.log("Hallo, Manuel!");
+}
