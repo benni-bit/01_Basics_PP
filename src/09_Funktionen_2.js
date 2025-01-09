d07a48c8832d8fed81b8e03100c87e23f2d527d5
benni-bit <benjaminotto.1@web.de>
2025-01-09 09:18:55 +0000

    09/division1. Iteration

---
+++ src/09_Funktionen_2.js
@@ -0,0 +1,56 @@
+/*** Rechner */
+/*
+0. a+b | a-b | a*b | a/b  --> Ergebnis c 
+
+1. Dateneingabe + -überprüfung :  
+2. Auswahl Rechenart : 
+3. Fkt. Grundrechenarten : 
+4. Ausgabe in Konsole : Test>>>>Check Module
+*/
+
+
+// module: division a / b |  test:
+// output(divide(4,2));
+// output(divide(3,2));
+// output(divide(3,-2));
+// output(divide(0,2));
+// output(divide(3,0));
+// output(divide(0,0));
+function divide(a,b) {
+    
+    return a / b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+
+
+// module: multiplication a * b |  test:
+//output(multiply(3,2));
+//output(multiply(3,-2));
+//output(multiply(3,0));
+ function multiply(a,b) {
+	return  a * b;
+}
+
+
+//module: subtraction a -b | test
+function sub(a,b) {
+    return a - b;
+}
+
+//module:addition a + b | test_
+//output(add(3,2));
+function add(a, b) {
+    return a + b; 
+}
+
+//module : output | test:
+//output("hi");
+
+function output(outputData) {
+    console.log(outputData);
+    
+}
