7ade81c42fae1f20fe4a8114ee541b8365451d2b
benni-bit <benjaminotto.1@web.de>
2025-01-10 12:03:11 +0000

    11_Arrays_1

--- src/11_Arrays_1.js
+++ src/11_Arrays_1.js
@@ -7,50 +7,47 @@
 
 /*** 02. Theorie: Array */
 
-//let arr;
-//arr = new Array();  //Konstruktor
+// let arr;
+// // arr = new Array(); // Konstruktor
+// arr = []; // Literal
+// arr = [2,11,7,8,10];
 
-//arr = [];          //Literalschreibweise
-//arr = [2,11,7,8,10];
-
-//output(arr);
-//output(arr.length);  //Anzahl der Elemente
-//output(arr[0]);      // Index 0 (1.Position im Array)
-//output(arr[arr.length -1]);       // letzte Position / allgemein
-
-
-// 02a. Theorie: Schleifen (for-schleife) */
-// For -Schleifen als allg. Wiederholungs-Struktur */
+// output(arr);
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]); // Index 0 (1. Pos. im Array)
+// output(arr[arr.length-1]); // letze Pos. / allgemein
 
 
-//for (let i = 0; i < 5; i++) 
-//{
-   //output("index: " + i);
-//}
+/* 02a. Theorie: Schleifen (for-schleife) */
+/* For -Schleifen als allg. Wiederholungs-Struktur */
 
+// Inkrement (untere Grenze --> obere Grenze)
+// for (let i = 0; i < 10; i++) 
+// {
+//     output("index: " + i);
+// }
 
 // Dekrement (obere Grenze --> untere Grenze)
-//for (let i = 10; i > 0; i--) 
-   // {
-   //     output("index: " + i);
-  //  }
+// for (let i = 10; i > 0; i--) 
+// {
+//     output("index: " + i);
+// }
     
-    // Inkrement (var. Schrittweite)
-//for (let i = 10; i <= 100; i += 5) 
-   // {
-    //   output("index: " + 1);   
-   // }
+// Inkrement (var. Schrittweite)
+// for (let i = 10; i <= 100; i += 5) 
+// {
+//     output("index: " + i);   
+// }
 
-   //2b. For -Schleife für Array-Index (Iteration)
+/* 02b. For-Schleife für Array-Index (Iteration)*/
+// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+// for (let i = 0; i < arr.length; i++) 
+// {
+//    output(i); 
+//    output(arr[i]);
+// }
 
-   let arr = ["Ich","bin","die","coole","Maxine","Mützerich","Falk"];
-for (let i = 0; i < arr.length; i++) 
-{
-   output(i); 
-   output(arr[i]);
-}
-
-//********* Überlegungen - Transponierung **********/
+/********* Überlegungen - Transponierung **********/
 
 /*
 1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
@@ -76,29 +73,17 @@ hier: Aufsummierung
 //    output("inhalt von a: " + a);
 // }
 
+/*
+1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+Solange die Variable existiert, bleibt dieser erhalten
+hier: Verkettung eines Strings // Transponierung
+*/
 
-//
-//1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
-//Solange die Variable existiert, bleibt dieser erhalten
-//hier: Verkettung eines Strings // Transponierung
-
-
- let str = ""; // Anfangswert
-for (let i = 0; i < 10; i++) {
-   str += "Test" + " "; // a = a + 1
-   output(str);
-}
-
-
-
-
-
-
-
-
-
-
-
+// let str = ""; // Anfangswert
+// for (let i = 0; i < 10; i++) {
+//    str += "Test" + " "; // a = a + 1
+//    output(str);
+// }
 
 
 
@@ -111,61 +96,75 @@ for (let i = 0; i < 10; i++) {
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
-//output(getSentence("Ich","bin","Max","Mütze"));
-
-//output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-
-
-function getSentence(word1,word2,word3,word4) {
+// output(getSentence("Ich","bin","Max","Mütze"));
+// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+/* function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
-    let str =   word1 + GAP +   // SRP SingleResponsibilityPrincipal | DRY -Don't repeat yourself!!!
+    let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
-                word4 +    
+                word4 + GAP +
+                word5 + GAP + 
+                word6 +
                 PUNCT;
 
     return str;
 }
+ */
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+// output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+/* function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP + 
+                arr[5] +
+                PUNCT;
 
-//1a. Funktionalität mit Array 1
-//Kritik | Überlegung
-// word1,word2 .....wordN:: Semantische Struktur
-//arr[0],arr[1]...arr[n]::: Numerische Struktur(0,1,2,3,4,.....n)
-//---> Transformation semantisches Problem--->numerisches Problem
-
-//output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
-//function getSentence(word1,word2,word3,word4) {  
-  //  const GAP = " ";
-  //  const PUNCT = ".";
-    //let str =   word1 + GAP +   // SRP SingleResponsibilityPrincipal | DRY -Don't repeat yourself!!!
-        //        word2 + GAP +
-        //        word3 + GAP +
-          //      word4 +    
-          //      PUNCT;
-
-    //return str;
-//}
-
-
+    return str;
+}
+ */
 /*** 01b. Funktionalität mit Array 2  */
 // Transponierung:  untereinander ---> nebeneinander
 // Helge Schneider: Anananandereihung ...
-// output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
+output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
 function getSentenceArr2(arr) {
     const GAP = " ";
     const PUNCT = ".";
     let str =  "";
 
-    let str = ""; // Anfangswert
-    for (let i = 0; i < 10; i++) {
-       str += "Test" + " "; // a = a + 1
-       output(str);
+    // 1. Variante: Struktur GENAU erzeugen ...
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (i != arr.length - 1) { // wenn du noch nicht am Ende des Arrays bist ...
+    //         str += arr[i] + GAP; // entweder ..
+    //     } else {
+    //         str += arr[i] + PUNCT;  // oder ...
+    //     }
+    // }
+
+    // return str;
+
+    // 2. Variante: Struktur erzeugen + trimmen()
+    for (let i = 0; i < arr.length; i++) {
+        str += arr[i] + GAP; 
     }
     
+    return str.trim() + PUNCT;
 
 
-    return str;
+
+}
+
+
+
 
 
 
@@ -174,6 +173,5 @@ function getSentenceArr2(arr) {
 function output(outputStr) {
     console.log(outputStr);
 }
-
 
 
